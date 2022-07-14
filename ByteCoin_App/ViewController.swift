d8c4823a4a4bc0f211af8359e4fc316fb3cf78e3
Lolaev Sergey <lolaevgasu@gmail.com>
2022-07-14 18:46:18 +0300

    initial commit

---
+++ ByteCoin_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ByteCoin_App
+//
+//  Created by admin on 14.07.2022.
+//  Copyright © 2022 Sergey Lolaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
